4999aad8266b88078148098c1f44574e20e59eeb
Jules Prévost <jprevostdev@gmail.com>
2020-04-23 12:37:48 +0200

    ameliorations of the animation + creation of mixins and functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,30 @@
-let title = document.getElementById('autotext');
-let text = title.innerHTML;
+/*Autotext animation*/
+
+let title = document.querySelectorAll('.autotext');
+let text = [].slice.call(title);
+
+text.forEach(function (title) {
+  let text = title.innerHTML;
 
 let index = 0;
 
 const randomSpeed = (min, max) => {
-	return Math.floor(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min) + min);
 };
 
 const play = () => {
-	title.innerHTML = text.slice(0, index)
+  title.innerHTML = text.slice(0, index);
 
-	index++;
+  index++;
 
-	if (index > text.length) {
-		index = 0;
-	}
+  if (index > text.length) {
+    index = 0;
+  }
 
-	clearInterval(timer);
-	timer = setInterval(play, randomSpeed(100,500));
+  clearInterval(timer);
+  timer = setInterval(play, randomSpeed(100,500));
 };
 
 let timer = setInterval(play, 300);
 
+});
